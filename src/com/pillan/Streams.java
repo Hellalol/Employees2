a5dbc5fad0a925309e60ac39f9138c6fb7061272
Fredrik Billing <frhabi@gmail.com>
2020-01-06 19:26:22 +0100

    More Streams

--- src/com/pillan/Streams.java
+++ src/com/pillan/Streams.java
@@ -3,6 +3,7 @@ package com.pillan;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Streams {
 
@@ -14,6 +15,8 @@ public class Streams {
                 "I26", "I17", "I29",
                 "O71");
 
+
+        // No streams solution
         List<String> gNumbers = new ArrayList<>();
 
         someBingoNumbers.forEach(number ->{
@@ -22,8 +25,34 @@ public class Streams {
                 //System.out.println(number);
             }
         });
-
         gNumbers.sort((String s1, String s2) -> s1.compareTo(s2));
         gNumbers.forEach((String s) -> System.out.println(s));
+
+
+        System.out.println("**********************");
+
+
+        // :: = Method reference
+        // used when calling an existing method
+
+
+        // Stream solution
+        someBingoNumbers
+                .stream()
+                .map(String::toUpperCase)
+                .filter(s -> s.startsWith("G"))
+                .sorted()
+                .forEach(System.out::println);
+
+        // Distinct removes dupes
+        // concatinating two streams with .concat
+        // count = count the objects
+
+        Stream<String> ioNumberStream = Stream.of("I26", "I17", "I29", "O71");
+        Stream<String> inNumberStream = Stream.of("N40", "N46", "I26", "I17", "I29", "O71");
+        Stream<String> concatStream = Stream.concat(ioNumberStream,inNumberStream);
+        System.out.println(concatStream.distinct().count());
+
     }
+
 }
